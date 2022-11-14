9946c6115097dab203a0541de5d91c3c025515f6
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-11-14 17:59:02 +0200

    task-02-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayOfListElements = ingredients.map(ingredient => {
+  const listItemEl = document.createElement('li');
+  listItemEl.classList.add('item');
+  listItemEl.textContent = ingredient;
+  return listItemEl;
+});
+
+document.querySelector('#ingredients').append(...arrayOfListElements);
