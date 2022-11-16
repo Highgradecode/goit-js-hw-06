82b65fead365d3792c1a57e729f27ebdcca33d2c
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-11-16 21:02:39 +0200

    task-08-done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector('.login-form')
 
+const loginFormFillCheck = (event) => {
+    event.preventDefault()
+    const ALERT_TEXT = 'Всі поля мають бути заповнені';
+    
+    const { email, password } = event.currentTarget.elements;
+    
+    if (email.value !== '' && password.value !== '') {
+        console.log({
+            email: email.value,
+            password: password.value
+        });
+
+        loginFormEl.reset()
+
+    } else alert(ALERT_TEXT)
+    
+    
+
+}
+
+loginFormEl.addEventListener('submit', loginFormFillCheck)
