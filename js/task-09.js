f35f907508310ae925b12900ccdc82b08c555e7b
Victoria <92218796+Highgradecode@users.noreply.github.com>
2022-11-16 21:05:39 +0200

    task-09-done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const colorName = document.querySelector('.color');
+const colorChangeButton = document.querySelector('.change-color');
+
+colorChangeButton.addEventListener("click", () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = bodyEl.style.backgroundColor;
+})
